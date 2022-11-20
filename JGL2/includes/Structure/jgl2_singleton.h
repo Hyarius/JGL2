e13a354affd0c559768569dd8334ba9c43b52fcb
Hyarius <Boissy.jeremy@yahoo.fr>
2022-11-20 19:29:19 +0100

    Adding singleton/SingletonWidget classes

---
+++ JGL2/includes/Structure/jgl2_singleton.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace jgl
+{
+	template <typename T>
+	class Singleton
+	{
+		/*
+		* Don't forget to add 'friend class Singleton<T>;' to T definition
+		*/
+	protected:
+		static inline T* _instance = nullptr;
+
+	public:
+		static T* instanciate()
+		{
+			if (_instance == nullptr)
+			{
+				_instance = new T();
+			}
+			return (_instance);
+		}
+		static T* instance()
+		{
+			return (_instance);
+		}
+		static void release()
+		{
+			delete _instance;
+		}
+	};
+}
